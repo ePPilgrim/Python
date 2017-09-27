5d7e0c4834a7b00ba5b06c6caed80462f27f2d62
Pavlo <demydyuk@gmail.com>
2017-09-27 15:09:28 -0700

    H4

--- TheCoinChangeProblem.py
+++ TheCoinChangeProblem.py
@@ -5,12 +5,13 @@ m=int(input())
 c=array('Q',[int(i) for i in input().split()])
 
 def solve(n, c):
-w=array('Q',[0 for i in range(0,n+1)])
-for i in c:
-for j in range(n,i-1):
-for k in range(j,i-1,i):
-w[j]+=w[k]
-return w[n]
+ w=array('Q',[0 for i in range(0,n+1)])
+ for i in c:
+  for j in range(n,i-1):
+   w[j]+=sum(w[k] for k in range(j,i-1,i))
+ return w[n]
 
+ res=solve(n,c)
+ 
   
 
