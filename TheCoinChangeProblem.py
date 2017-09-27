8e38a11cb4244e1c8fa5c67301d969f7297af534
Pavlo <demydyuk@gmail.com>
2017-09-27 15:03:14 -0700

    H2

--- TheCoinChangeProblem.py
+++ TheCoinChangeProblem.py
@@ -7,5 +7,10 @@ c=array('Q',[int(i) for i in input().split()])
 def solve(n, c):
 w=array('Q',[0 for i in range(0,n+1)])
 for i in c:
-k=n//i
+for j in range(n,i-1):
+for k in range(j,i-1,i):
+w[j]+=w[k]
+return w[n]
+
+  
 
