38357ed15e156834b69aa350adac3d6669b864e2
Pavlo <demydyuk@gmail.com>
2017-09-29 14:28:59 -0700

    New task

--- AngryChildren2.py
+++ AngryChildren2.py
@@ -4,21 +4,22 @@ N=int(input())
 K=int(input())
 W=[int(i) for i in input().split()]
 
-def solve(k,w)
- w=[0]+w.sort()
+def solve(k,w):
+ w.sort()
+ w=[0]+w
  s=w
- for i in range(1,len(s))
+ for i in range(1,len(s)):
   s[i]+=s[i-1]
  min=0
- for i in range(1,k)
+ for i in range(1,k):
   min+=s[k]-2*s[i]
  sum = min
- for i in range(k+1, len(s))  
-  sum = sum - 2*(s[i-1] - s[i-k]) + (k-1)*(w[i]-w[i-k])
-  if sum < min
+ for i in range(k+1, len(s)):
+  sum = sum - 2*(s[i-1] - s[i-k]) + (k-1)*(w[i]+w[i-k])
+  if sum < min:
    min = sum
  return min
- 
- print(solve(K,W))
+
+print(solve(K,W))
 
 
