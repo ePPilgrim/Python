c3c885217b4748e8d0b63040065f30a1c5d9854f
Pavlo <demydyuk@gmail.com>
2017-10-11 15:02:42 -0700

    P4_1-compleate

--- ML/ProblemSet4/problem1.py
+++ ML/ProblemSet4/problem1.py
@@ -1,16 +1,35 @@
 import numpy as np
 import numpy.random as npr
+import numpy.linalg as lin
 import matplotlib.pyplot as plt
 
+def fun_a(n):
+ pts = npr.random((n, 3)) - 0.5
+ return pts
+
+def fun_b(pts, nx,n):
+ x = np.linspace(0.001, 1, nx)
+ dest=np.array([np.count_nonzero(np.all(np.abs(pts)<=i,1)) for i in x])
+ denum = n * (x**3)
+ y = dest / denum
+ plt.plot(x,y)
+ plt.show()
+
+def fun_c(pts,n):
+ x = lin.norm(pts,ord=2,axis=1)
+ x.sort()
+ y = 3*(np.arange(n)+1)/(4*n*np.pi*(x**3))
+ plt.plot(x,y)
+ plt.show()
+
+def fun_d(n):
+ pts = npr.randn(n,3)
+ return pts
+
 n = 20000
-pts = npr.random((n, 3)) - 0.5
-num_x = 1000
-h_x = np.linspace(0.001, 1, num_x)
-dest=np.array([np.count_nonzero(np.all(np.abs(pts)<=i,1)) for i in h_x])
-denum = n * (h_x**3)
-y = dest / denum
-plt.plot(h_x,y)
-plt.show()
+nx = 1000
+
+
 
 
 
