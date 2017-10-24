cd68a26252d83cbb20b332049907ef726036f6e1
Pavlo <demydyuk@gmail.com>
2017-10-23 22:00:00 -0700

    sdfsd

--- ML/ProblemSet4/problem4.py
+++ ML/ProblemSet4/problem4.py
@@ -14,9 +14,16 @@ trset=trset/norms
 
 
 class VorClassifier:
- def __init__(self,vertexes):
+ def __init__(self,vertexes, cat):
   rix = np.arrange(30)
   self.vor2d = Voronoi(trset[(rix<10)^(rix>=20),:2])
-  self.vor3d = Voronoi(trset)
+  self.cat = cat
+  
+ def getDenseVor(self):
+  ridge_points = self.vor2d.ridge_points
+  lx = 
+  for i in np.max(ridge_points]
+  
+
 
 
