ad5930b9c53b3729ef2e17d920492a21d9b1bdcd
Pavlo Demydiuk <PLDD@simcorp.com>
2017-10-24 17:49:39 +0300

    Add problem5

---
+++ ML/ProblemSet4/problem5.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy.random as npr
+import numpy.linalg as lin
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
