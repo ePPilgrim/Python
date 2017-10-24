18fa5943ed48dc87b99488f6a2d95d905eb92178
Pavlo Demydiuk <PLDD@simcorp.com>
2017-10-24 13:10:17 +0300

    asdfasdf

--- ML/ProblemSet4/problem4.py
+++ ML/ProblemSet4/problem4.py
@@ -18,11 +18,24 @@ class VorClassifier:
   rix = np.arrange(30)
   self.vor2d = Voronoi(trset[(rix<10)^(rix>=20),:2])
   self.cat = cat
-  
+
  def getDenseVor(self):
   ridge_points = self.vor2d.ridge_points
-  lx = 
-  for i in np.max(ridge_points]
+  ridge_cat = self.cat[ridge_points]
+  lx = ridge_cat[:,1]!=ridge_cat[:,2]
+  points = ridge_points(np.unique(ridge_points[lx,:].ravel()))
+  self.vor2dpur = Voronoi(points)
+
+  def draw(self):
+   voronoi_plot_2d(self.vor2d)
+   plt.show()
+   voronoi_plot_2d(self.vor2dpur)
+   plt.show()
+
+cat = np.zeros(20)
+cat[10:20]=1
+
+VorClass = VorClassifier(trset,cat)
   
 
 
