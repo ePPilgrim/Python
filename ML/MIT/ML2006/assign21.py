d8a918b1581d505ebd8fb20dcdefcc1cffac62d5
Pavlo Demydiuk <PLDD@simcorp.com>
2017-11-21 11:22:54 +0200

    BBBB

--- ML/MIT/ML2006/assign21.py
+++ ML/MIT/ML2006/assign21.py
@@ -28,9 +28,27 @@ def active_learn(X, k1, k2):
   np.append(idx,idx_[np.argmax(v)])
  return idx
 
+def calc_mse(y,thau,X):
+ return np.sum((np.dot(X, thau) - y)**2)/y.size
+
 class LinReg:
- def __init__(self,n):
+ def __init__(self,n=1000):
   self.X = npr.uniform(-1.0, 1.0, (n, 3))
   self.y_true = (np.sum(np.tile([-10.0, -15.0, -7.5],(n,1)) * np.log(self.X),axis = 1) + 1)*2.0
   self.y_noise = self.y_true + 10.0*npr.randn(n)
+
+ def solvec(self):
+  X1 = feature_mapping(self.X,1)
+  X2 = feature_mapping(self.X,2)
+  idx1 = active_learn(X1, 5, 10)
+  idx2 = active_learn(X2, 5, 10)
+  thau1 = np.ravel(linear_regress(self.y_noise[idx1],X2[idx1,:]))
+  thau2 = np.ravel(linear_regress(self.y_noise[idx2],X2[idx2,:]))
+  y1 = linear_pred(thau1, X2)
+  y2 = linear_pred(thau2, X2)
+  err1 = np.sum((self.y_true - y1)**2)
+  err2 = np.sum((self.y_true - y2)**2)
+  return (err1, err2)
+
+
 
