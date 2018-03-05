03556ab0ca3ff9978aebe31a213943f7dc7831a5
Pavlo Demydiuk <PLDD@simcorp.com>
2018-03-05 18:24:01 +0200

    correct KingdomDiv

--- HackerRank/KingdomDiv.py
+++ HackerRank/KingdomDiv.py
@@ -3,37 +3,34 @@ import sys
 
 prime = int(10e9 + 7)
 
+
 def kingdomDivision(roads):
     (res, sink) = Solve(1, roads)
     return (res + res) % prime
 
 
-def Solve(nik, graph):
-    node = graph[nik]
-    graph[nik] = []
-    war, peace, cnt = 1, 1, 0
-    for ik in node:
-        if graph[ik] == []: 
-            continue
-        cnt += 1
-        x, y = Solve(ik, graph)
-        peace = (peace * (x + y)) % prime
-        war = (war * x) % prime
-    if cnt != 0:
-        return ((prime + peace - war) % prime, peace)
-    return (0, 1)
-
-
-def FindSolution():
-    n = int(input().strip())
-    roads = [[]] * (n + 1)
-    for roads_i in range(n-1):
-       roads_t = [int(roads_temp) for roads_temp in input().strip().split(' ')]
-       roads[roads_t[0]].append(roads_t[1])
-       roads[roads_t[1]].append(roads_t[0])
-    roads=[[], [2,3],[1],[1,4,5],[3], [3]]
-    return kingdomDivision(roads)
+def Solve(graph):
+    while True:
+        x, y = 0, 0
+        for i in graph:
+            if len(graph[i][2]) == 1:
+                x = graph[i][0]
+                y = graph[i][1]
+                x = (prime + y - x) % prime
+                ik = graph[i][2][0]
+                graph[ik][0] = (graph[ik][0] * x) % prime
+                graph[ik][1] = (graph[ik][1] * ((x + y) % prime)) % prime
+                graph[i][2] = []
+                graph[ik][2].remove(i)
+            elif 
+              
     
 
 if __name__ == "__main__":
-    print(FindSolution())
+    n = int(input().strip())
+    roads = [[[1],[1],[]] for i in range(n + 1)]
+    for roads_i in range(n-1):
+       roads_t = [int(roads_temp) for roads_temp in input().strip().split(' ')]
+       roads[roads_t[0]][2].append(roads_t[1])
+       roads[roads_t[1]][2].append(roads_t[0])
+    print(kingdomDivision(roads))
