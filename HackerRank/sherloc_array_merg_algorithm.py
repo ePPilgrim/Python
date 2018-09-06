62b05011e629101217086b9e91d813721d1e5e9e
unknown <p.demydiuk@gmail.com>
2018-09-06 08:03:33 +0300

    Sherloc_Array_merg

--- HackerRank/sherloc_array_merg_algorithm.py
+++ HackerRank/sherloc_array_merg_algorithm.py
@@ -1,24 +1,21 @@
 
 def arrayMerging(m):
     prime = 1000000007
-    m += [-1]
     N = len(m)
-    arr2D = [[0]*N for i in range(N)]
-    arr2D[N - 1][:] = [1] * len(arr2D[N - 1][:])
-    fac = [i for i in range(N)]
-    fac[0] = 1
-    for i in range(1, N):
-        fac[i] = (fac[i] * fac[i - 1]) % prime
-    for i in range(N - 2,-1,-1):
-            arr2D[i][1] = 1
-            n = 1
-            for j in range(i + 1,N):
-                if m[j] < m[j - 1]: break
-                n = j - i
-                arr2D[i][n] = (n * arr2D[j][n]) % prime
-            for j in range(n + 1, N):
-                arr2D[i][j] = arr2D[i][n]
-    print(arr2D)
+    arr2D = [[0] * (N - i) for i in range(N)]
+    pps = [N - 1]
+    for i in range(N - 2, -1):
+        if m[i] > m[i + 1]: pps = [i] + pps
+        else : pps = [pps[0]] + pps
+    perm = [[0] * N for i in range(N)]
+    for i in range(N):
+        for j in range(i + 1):
+            if j != 0: perm[i][j] = (perm[i - 1][j - 1] * (i + 1)) % prime
+            else : perm[i][0] = i
+    
+    for i in range(N - 1, -1):
+        for j in range(pps[i] - i + 1):
+            
     return sum(arr2D[0])
                 
 m_count = int(input())
