fd8ec59d8cf0bbf626e9ca3654ec18558da2e33b
PLDD <p.demydiuk@gmail.com>
2020-09-05 16:42:34 +0300

    Add new c# src and models

---
+++ MHS/src/logicalform/TFService/LogicalForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFService
+{
+//    public class ModernHistoricalSimulationLogicalForm : ILogicalForm, IDisposable
+//    {
+
+//        private double iter = 0.0;
+
+//        private readonly ApplicationBinding<ModernHistoricalSimulation> Binding;
+
+//        public CommandElement Fetch { get; set; }
+//        public CommandElement Run { get; set; }
+//        public TypedElement<double> KLD { get; set; }
+//        public ListView<DistributionGrid> Grid { get; set; }
+
+//        public ModernHistoricalSimulationLogicalForm(ILogicalViewController controller, IViewElementFactory factory, IAgent agent)
+//        {
+//            Binding = ApplicationBinding<ModernHistoricalSimulation>.CreateAndInitialize(controller, agent);
+
+//            Grid = factory.NewGridElement(controller.CreateControlledView<DistributionGrid>(nameof(Grid)))
+//            .WithSelectedColumns(new[] {
+//                    nameof(DistributionGrid.Original),
+//                    nameof(DistributionGrid.Approximation),
+//                    nameof(DistributionGrid.Deviation)
+//            }).Build();
+
+//            Fetch = factory.NewCommandElement().WithTitle("Fetch Data").Build();
+//            Fetch.DoExecute += OnFetchData;
+
+//            Run = factory.NewCommandElement().WithTitle("Sent Request").Build();
+
+//            KLD = factory.NewTypedElement<double>().WithMaxLength(10).WithTitle("KLD distance").Build();
+
+//            controller.InitializeView(this);
+//        }
+
+//        private void OnFetchData(object sender, CommandEventArgs e)
+//        {
+//            if (Grid.Items.Any()) { Grid.DeleteItems(Grid.Items.Select(item => item.Id).ToList()); }
+//            var data = new[] {
+//                    new {Original = 0.1, Approximated = 0.2},
+//                    new {Original = 0.3, Approximated = 0.4},
+//                    new {Original = 0.5, Approximated = 0.6}
+//                }.Select(x => new { Original = x.Original + iter, Approximated = x.Approximated + iter }).ToList();
+//            Grid.AddItems(data);
+//            iter += 1.0;
+//        }
+            
+//            public void Dispose()
+//        {
+//            //throw new NotImplementedException();
+//        }
+
+//        public TypedElement<double> Original { get; }
+//        public TypedElement<double> Approximation { get; }
+//        public TypedElement<double> Deviation { get; }⌈
+//        }
+//}
+}
---
+++ MHS/src/logicalform/TFService/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFService
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Convert.ToDouble("0.003341", System.Globalization.CultureInfo.InvariantCulture);
+            var obj = new TFService(@"C:\Users\PLDD\Practice\GitHub\Python\MHS\data\train\train_real_0_0_410_38_40938.txt", "http://localhost:8501/v1/models/my_model:predict", 40930);
+            obj.FetchData();
+            var predicted = obj.GetPostData();
+            var cashflows = obj.MergeToGrid(obj.Target, predicted);
+            var vars = obj.VarToGrid(cashflows);
+            foreach(var val in vars)
+            {
+                Console.WriteLine($"{val.Risk} -- {val.TrueVar} -- {val.PredictedVar}");
+            }
+
+        }
+    }
+}
