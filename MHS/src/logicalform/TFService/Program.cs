89360d1cd32a17b7e5dffb16762464da693274a4
PLDD <p.demydiuk@gmail.com>
2020-09-06 13:27:33 +0300

    Some changes

--- MHS/src/logicalform/TFService/Program.cs
+++ MHS/src/logicalform/TFService/Program.cs
@@ -13,14 +13,52 @@ namespace TFService
         static void Main(string[] args)
         {
             var num = Convert.ToDouble("0.003341", System.Globalization.CultureInfo.InvariantCulture);
-            var obj = new TFService(@"C:\Users\PLDD\Practice\GitHub\Python\MHS\data\train\train_real_0_0_410_38_40938.txt", "http://localhost:8501/v1/models/my_model:predict", 40930);
+            var obj = new TFService(@"C:\Users\PLDD\Practice\GitHub\Python\MHS\data\train\repository\test_102229", "http://localhost:8501/v1/models/my_model:predict", 102000);
             obj.FetchData();
-            var predicted = obj.GetPostData();
+            var obs = obj.Observables.instances;
+            //foreach(var val1 in obs)
+            //{
+            //    foreach(var val2 in val1)
+            //    {
+            //        Console.Write($"{val2}, ");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //foreach(var val in obj.Target)
+            //{
+            //    Console.Write($"{val}, ");
+            //}
+            //Console.WriteLine("");
+            var predicted = obj.GetPostData().ToList();
+            //foreach (var val in predicted)
+            //{
+            //    Console.Write($"{val}, ");
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine(obj.jjson);
+            var rnd = new Random();
+            
             var cashflows = obj.MergeToGrid(obj.Target, predicted);
-            var vars = obj.VarToGrid(cashflows);
+
+            int i = 0;
+            foreach(var val in cashflows)
+            {
+                var pos = rnd.Next(0, 3000);
+                if( pos % 2 == 0)
+                {
+                    Console.WriteLine($"{val}");
+                    i++;
+                }
+                if(i > 10)
+                {
+                    break;
+                }
+            }
+            var vars = obj.VarToGrid(obj.Target.ToList(), predicted);
             foreach(var val in vars)
             {
-                Console.WriteLine($"{val.Risk} -- {val.TrueVar} -- {val.PredictedVar}");
+                Console.WriteLine($"{val}");
             }
 
         }
